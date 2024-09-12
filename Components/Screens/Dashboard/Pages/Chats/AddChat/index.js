b4b2466eeb389799d069f93f97a2332492f4d9cc
1-Pankaj <pankajisrani17@gmail.com>
2024-09-12 16:33:58 +0530

    Installed Contacts package and Updates AddChat Page

--- Components/Screens/Dashboard/Pages/Chats/AddChat/index.js
+++ Components/Screens/Dashboard/Pages/Chats/AddChat/index.js
@@ -5,11 +5,16 @@ import { ExpandableSection } from "react-native-ui-lib"
 import { useEffect, useRef, useState } from "react"
 import { Ionicons, MaterialIcons } from "@expo/vector-icons"
 import TextRegular from "../../../../../UIElements/TextRegular"
+import { Text } from "react-native-paper"
+import TouchableScale from "@jonny/touchable-scale"
+import DarkColours from "../../../../../Themes/DarkColours"
+import LightColours from "../../../../../Themes/LightColours"
 
 export default AddChat = (props) => {
 
     const [expanded, setExpanded] = useState(true)
     const [themeState, setThemeState] = useState(Appearance.getColorScheme())
+    const [searchText, setSearchText] = useState("")
 
     const animatedWidth = useState(new Animated.Value(Dimensions.get('window').width - 30))[0]
 
@@ -17,7 +22,7 @@ export default AddChat = (props) => {
 
     const animateSearchBar = () => {
         Animated.timing(animatedWidth, {
-            toValue: focused ?
+            toValue: searchText ?
                 Dimensions.get('window').width - 80
                 :
                 Dimensions.get('window').width - 30,
@@ -30,7 +35,7 @@ export default AddChat = (props) => {
 
     useEffect(() => {
         animateSearchBar()
-    }, [focused])
+    }, [searchText])
 
     useEffect(() => {
         Appearance.addChangeListener(() => {
@@ -74,24 +79,76 @@ export default AddChat = (props) => {
                                     setFocused(true)
                                 }} onBlur={() => {
                                     setFocused(false)
-                                }} autoFocus ref={searchRef}/>
+                                }} autoFocus ref={searchRef} onChangeText={(tx) => {
+                                    setSearchText(tx)
+                                }}
+                                    value={searchText} />
                             </Animated.View>
-                            <TouchableOpacity onPress={()=>{
+                            <TouchableOpacity onPress={() => {
                                 searchRef.current.blur()
-                            }} style={{marginStart:20,
-                                width:30, height:30, backgroundColor:themeState === 'dark' ? '#212121' : '#f0f0f0',
+                                setSearchText("")
+                            }} style={{
+                                marginStart: 20,
+                                width: 30, height: 30, backgroundColor: themeState === 'dark' ? '#212121' : '#f0f0f0',
                                 borderRadius: 50, justifyContent: 'center', alignItems: 'center',
                             }} activeOpacity={0.6
-                                
+
                             }>
-                                <MaterialIcons name="close"  size={18}
-                                color={themeState === 'dark'?
-                                    'white' : 'black'
-                                }/>
+                                <MaterialIcons name="close" size={18}
+                                    color={themeState === 'dark' ?
+                                        'white' : 'black'
+                                    } />
                             </TouchableOpacity>
                         </View>
                     </ExpandableSection>
                 </Animated.View>
+                <View style={{
+                    width: '92%',
+                    flexDirection: 'row', alignItems: 'center',
+                    justifyContent: 'space-between', alignSelf: 'center'
+                }}>
+                    <TouchableScale style={{ width: '46%' }}>
+                        <View style={{
+                            marginTop: 20, width: '100%', height: 55,
+                            backgroundColor: themeState === 'dark' ? '#212121' : '#f0f0f0',
+                            borderRadius: 10, flexDirection: 'row',
+                            alignItems: 'center', justifyContent: 'center'
+                        }}>
+                            <View style={{
+                                width: 40, height: 40, backgroundColor:
+                                    themeState === 'dark' ? '#414141' : 'lightgray',
+                                alignItems: 'center', justifyContent: 'center',
+                                borderRadius: 20
+                            }}>
+                                <MaterialIcons name="person-add" size={22}
+                                    color={themeState === 'dark' ? 'white' : 'black'} />
+                            </View>
+                            <TextRegular value={"New Contact"} fontSize={16}
+                            marginStart={10}/>
+                        </View>
+                    </TouchableScale>
+                    <TouchableScale style={{ width: '46%' }}>
+                        <View style={{
+                            marginTop: 20, width: '100%', height: 55,
+                            backgroundColor: themeState === 'dark' ? '#212121' : '#f0f0f0',
+                            borderRadius: 10, flexDirection: 'row',
+                            alignItems: 'center', justifyContent: 'center'
+                        }}>
+                            <View style={{
+                                width: 40, height: 40, backgroundColor:
+                                    themeState === 'dark' ? '#414141' : 'lightgray',
+                                alignItems: 'center', justifyContent: 'center',
+                                borderRadius: 20
+                            }}>
+                                <MaterialIcons name="group-add" size={22}
+                                    color={themeState === 'dark' ? 'white' : 'black'} />
+                            </View>
+                            <TextRegular value={"New Group"} fontSize={16}
+                            marginStart={10}/>
+                        </View>
+                    </TouchableScale>
+
+                </View>
             </View>
         </View>
     )
